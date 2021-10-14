44de904bf8b2bcdaa6b3aef8fa127f54884b35d4
Ring <ring@ringdembp.attlocal.net>
2021-10-14 14:44:02 -0700

    save work

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 // 1. Set the submit button to send the email
 // 2. Add the text input into the mailing input
 // 3. sent it out
+// $('.email').on('click',function(){
+//     <a href="mailto:Czhring@gmail.com" class = 'email'></a>
+// })
 
 
-$('.email').on('click',function(){
-    <a href="mailto:Czhring@gmail.com" class = 'email'></a>
+
+$('.first').mouseenter(function(){
+    $(this).toggleClass('active')
+})
+$('.second').mouseenter(function(){
+    $(this).toggleClass('active')
+})
+$('.third').mouseenter(function(){
+    $(this).toggleClass('active')
+})
+$('.fourth').mouseenter(function(){
+    $(this).toggleClass('active')
+})
+$('.fifth').mouseenter(function(){
+    $(this).toggleClass('active')
+})
+
+$('.first').mouseleave(function(){
+    $(this).toggleClass('active2')
+})
+$('.second').mouseleave(function(){
+    $(this).toggleClass('active2')
+})
+$('.third').mouseleave(function(){
+    $(this).toggleClass('active2')
+})
+$('.fourth').mouseleave(function(){
+    $(this).toggleClass('active2')
+})
+$('.fifth').mouseleave(function(){
+    $(this).toggleClass('active2')
 })
