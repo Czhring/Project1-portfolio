7a3297bf303c6be6cb25e058dff068a26bed4b67
Ring <ring@ringdembp.attlocal.net>
2021-10-14 11:24:18 -0700

    save work

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1. Set the submit button to send the email
+// 2. Add the text input into the mailing input
+// 3. sent it out
+
+
+$('.email').on('click',function(){
+    <a href="mailto:Czhring@gmail.com" class = 'email'></a>
+})
